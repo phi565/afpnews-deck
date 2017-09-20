a8e04aab7a0c5e0f3a6ed17078159842ea5c1287
Clement Procureur <clement@sgrx04.afp.local>
2017-09-20 11:29:48 +0200

    export and build process

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export { default as External } from './components/External.vue'
+
+export { default as myModule } from './store/mymodule.js'
+
+export * from './config'
+
+export * from './config/getters'
+
+export * from './config/actions'
+
+export * from './config/mutations'
