f3f8e3501804fa8491139a3ddd05103b1edc767b
Jules Bonnard <julesbonnard@gmail.com>
2018-03-09 18:17:29 +0100

    only export modified columns

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,11 @@
 <template>
   <div id="app">
     <afp-deck
-      :columns="columns"
-      @addColumn="addColumn"
-      @moveColumn="moveColumn"
-      @closeColumn="closeColumn"
-      @refreshColumn="refreshColumn"
-      @resetColumn="resetColumn"
-      @loadMoreDocuments="loadMoreDocuments"
-      :autoRefresh.sync="autoRefresh"
-      @columnParamsChanged="columnsParamsChanged"
-      @columnNameChanged="columnsNameChanged"
+      ref="afpDeck"
+      :api="api"
+      :initialColumns="columns"
+      @saveColumns="saveColumns"
+      @saveToken="saveToken"
     ></afp-deck>
   </div>
 </template>
@@ -20,15 +15,6 @@
 import AfpNews from 'afpnews-api'
 import AfpDeck from './components/AfpDeck'
 
-const defaultSearchParams = {
-  dateFrom: '2012-01-01',
-  dateTo: 'now',
-  urgency: null,
-  lang: 'fr',
-  size: 10,
-  searchTerms: ''
-}
-
 export default {
   name: 'app',
   components: {
@@ -36,24 +22,12 @@ export default {
   },
   data () {
     return {
-      _afpNews: null,
-      columns: [],
-      autoRefreshTimer: null,
-      autoRefresh: false,
-      autoRefreshDelay: 5000
+      api: undefined,
+      columns: []
     }
   },
-  created () {
+  mounted () {
     this.init()
-  },
-  beforeDestroy () {
-    this.stopAutoRefresh()
-  },
-  watch: {
-    autoRefresh (newVal) {
-      if (newVal) this.startAutoRefresh()
-      else this.stopAutoRefresh()
-    }
   },
   methods: {
     async init () {
@@ -62,140 +36,38 @@ export default {
       const clientSecret = localStorage.getItem('afpnews-clientsecret') || prompt('Please enter your client Secret', null)
       localStorage.setItem('afpnews-clientsecret', clientSecret)
 
-      this._afpNews = new AfpNews({ baseUrl: 'https://api.afpforum.com', clientId, clientSecret })
+      this.api = new AfpNews({ baseUrl: 'https://api.afpforum.com', clientId, clientSecret })
 
       const token = localStorage.getItem('afpnews-token')
 
       if (token) {
-        this._afpNews.token = JSON.parse(token)
+        this.api.token = JSON.parse(token)
       } else {
         const username = prompt('Please enter your username', null)
         const password = prompt('Please enter your password', null)
-        await this._afpNews.authenticate({
+        await this.api.authenticate({
           username,
           password
         })
       }
-      localStorage.setItem('afpnews-token', JSON.stringify(this._afpNews.token))
+      localStorage.setItem('afpnews-token', JSON.stringify(this.api.token))
 
       this.initColumns()
     },
-    async refreshColumn (indexCol, more) {
-      let params = JSON.parse(JSON.stringify(this.columns[indexCol].params))
-
-      if (more === 'before') {
-        const lastDocument = this.columns[indexCol].documents.slice(-1).pop()
-        if (lastDocument) {
-          let lastDate = new Date(lastDocument.published)
-          lastDate.setSeconds(lastDate.getSeconds() - 1)
-          params = Object.assign(params, { dateTo: lastDate.toISOString(), dateFrom: '2012-01-01' })
-        }
-      } else if (more === 'after') {
-        const firstDocument = this.columns[indexCol].documents[0]
-        if (firstDocument) {
-          let firstDate = new Date(firstDocument.published)
-          firstDate.setSeconds(firstDate.getSeconds() + 1)
-          params = Object.assign(params, { dateFrom: firstDate.toISOString(), dateTo: 'now' })
-        }
-      }
-
-      this.columns[indexCol].processing = true
-
-      try {
-        const { documents, count } = await this._afpNews.search(params)
-        localStorage.setItem('afpnews-token', JSON.stringify(this._afpNews.token))
-
-        const existingDocuments = this.columns[indexCol].documents
-
-        if (more === 'before') {
-          this.columns[indexCol].documents = existingDocuments.concat(documents)
-        } else if (more === 'after') {
-          this.columns[indexCol].documents = documents.concat(existingDocuments)
-        } else {
-          this.columns[indexCol].documents = documents
-          this.columns[indexCol].documentsCount = count
-        }
-
-        this.columns[indexCol].error = false
-      } catch (e) {
-        console.error(e)
-        this.columns[indexCol].error = true
-        this.columns[indexCol].documents = []
-      }
-
-      this.columns[indexCol].processing = false
-    },
-    addColumn (name = 'Default', params = {}, paramsOpen = true) {
-      params = Object.assign(JSON.parse(JSON.stringify(defaultSearchParams)), params)
-
-      this.columns.push({
-        name,
-        params,
-        documents: [],
-        documentsCount: 0,
-        processing: false,
-        error: false,
-        paramsOpen
-      })
-      this.saveColumns()
-    },
-    moveColumn (indexCol, dir) {
-      const to = dir === 'left' ? indexCol - 1 : indexCol + 1
-      const element = this.columns[indexCol]
-      this.columns.splice(indexCol, 1)
-      this.columns.splice(to, 0, element)
-      this.saveColumns()
-    },
-    closeColumn (index) {
-      const confirmation = confirm('Do you really want to delete the column ?')
-      if (!confirmation) return false
-      this.columns.splice(index, 1)
-      this.saveColumns()
-    },
-    async loadMoreDocuments (indexCol) {
-      await this.refreshColumn(indexCol, 'before')
-    },
-    resetColumn (indexCol) {
-      this.columns[indexCol].documents = []
-      this.columns[indexCol].count = 0
-    },
-    startAutoRefresh () {
-      this.autoRefreshTimer = setInterval(() => {
-        Promise.all(
-          this.columns
-            .filter(column => !column.paramsOpen)
-            .map((column, i) => this.refreshColumn(i, 'after'))
-        )
-      }, this.autoRefreshDelay)
-    },
-    stopAutoRefresh () {
-      if (this.autoRefreshTimer) clearInterval(this.autoRefreshTimer)
-    },
-    saveColumns () {
-      const savedColumns = this.columns.map(column => {
-        return {
-          params: column.params,
-          name: column.name
-        }
-      })
+    saveColumns (savedColumns) {
       localStorage.setItem('afpnews-columns', JSON.stringify(savedColumns))
     },
-    columnsParamsChanged (indexCol) {
-      // this.columns[indexCol].documents = []
-      // this.columns[indexCol].count = 0
-      this.saveColumns()
-    },
-    columnsNameChanged (indexCol) {
-      this.saveColumns()
+    saveToken (savedToken) {
+      localStorage.setItem('afpnews-token', JSON.stringify(savedToken))
     },
     initColumns () {
       if (localStorage.getItem('afpnews-columns')) {
         const savedColumns = JSON.parse(localStorage.getItem('afpnews-columns'))
         savedColumns.forEach(({ name, params }) => {
-          this.addColumn(name, params, false)
+          this.$refs.afpDeck.addColumn(name, params, false)
         })
       } else {
-        this.addColumn(undefined, undefined, false)
+        this.$refs.afpDeck.addColumn(undefined, undefined, false)
       }
     }
   }
--- src/components/AfpDeck.vue
+++ src/components/AfpDeck.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="afpdeck">
-    <side-bar @addColumn="$emit('addColumn')" :autoRefresh="autoRefresh" @update:autoRefresh="val => { $emit('update:autoRefresh', val) }"></side-bar>
+    <side-bar @addColumn="addColumn" :autoRefresh.sync="autoRefresh"></side-bar>
     <div id="columns">
       <column
         v-for="(column, i) in columns"
@@ -9,15 +9,15 @@
         :documentsCount="column.documentsCount"
         :processing="column.processing"
         :error="column.error"
-        @close="$emit('closeColumn', i)"
-        @move="val => { $emit('moveColumn', i, val) }"
-        @refresh="$emit('refreshColumn', i)"
-        @reset="$emit('resetColumn', i)"
-        @loadMore="$emit('loadMoreDocuments', i)"
+        @close="val => { closeColumn(i) }"
+        @move="val => { moveColumn(i, val) }"
+        @refresh="val => { refreshColumn(i) }"
+        @reset="val => { resetColumn(i) }"
+        @loadMore="val => { loadMoreDocuments(i) }"
         :params.sync="column.params"
-        @update:params="$emit('columnParamsChanged')"
+        @update:params="saveColumns"
         :name.sync="column.name"
-        @update:name="$emit('columnNameChanged')"
+        @update:name="saveColumns"
         :paramsOpen.sync="column.paramsOpen">
       </column>
     </div>
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import AfpNews from 'afpnews-api'
 import Vue from 'vue'
 import bus from '@/utils/bus'
 import SideBar from '@/components/SideBar'
@@ -41,26 +42,155 @@ import VueLinkify from 'vue-linkify'
 
 Vue.directive('linkified', VueLinkify)
 
+const defaultSearchParams = {
+  dateFrom: '2012-01-01',
+  dateTo: 'now',
+  urgency: null,
+  lang: 'fr',
+  size: 10,
+  searchTerms: ''
+}
+
 export default {
   name: 'afp-deck',
   props: {
-    columns: {
-      type: Array
+    api: {
+      type: Object,
+      default: () => {
+        return new AfpNews()
+      }
     },
-    autoRefresh: {
-      type: Boolean
+    initialColumns: {
+      type: Array,
+      default: []
     }
   },
   components: { SideBar, Column, Modal },
   data () {
     return {
-      currentDocument: null
+      currentDocument: null,
+      columns: this.initialColumns,
+      autoRefreshTimer: null,
+      autoRefresh: false,
+      autoRefreshDelay: 5000
     }
   },
   created () {
     bus.$on('setCurrentDocument', data => {
       this.currentDocument = data
     })
+  },
+  watch: {
+    autoRefresh (newVal) {
+      if (newVal) this.startAutoRefresh()
+      else this.stopAutoRefresh()
+    }
+  },
+  beforeDestroy () {
+    this.stopAutoRefresh()
+  },
+  methods: {
+    addColumn (name = 'Default', params = {}, paramsOpen = true) {
+      params = Object.assign(JSON.parse(JSON.stringify(defaultSearchParams)), params)
+
+      this.columns.push({
+        name,
+        params,
+        documents: [],
+        documentsCount: 0,
+        processing: false,
+        error: false,
+        paramsOpen
+      })
+      this.saveColumns()
+    },
+    moveColumn (indexCol, dir) {
+      const to = dir === 'left' ? indexCol - 1 : indexCol + 1
+      const element = this.columns[indexCol]
+      this.columns.splice(indexCol, 1)
+      this.columns.splice(to, 0, element)
+      this.saveColumns()
+    },
+    closeColumn (index) {
+      const confirmation = confirm('Do you really want to delete the column ?')
+      if (!confirmation) return false
+      this.columns.splice(index, 1)
+      this.saveColumns()
+    },
+    resetColumn (indexCol) {
+      this.columns[indexCol].documents = []
+      this.columns[indexCol].count = 0
+    },
+    async refreshColumn (indexCol, more) {
+      let params = JSON.parse(JSON.stringify(this.columns[indexCol].params))
+
+      if (more === 'before') {
+        const lastDocument = this.columns[indexCol].documents.slice(-1).pop()
+        if (lastDocument) {
+          let lastDate = new Date(lastDocument.published)
+          lastDate.setSeconds(lastDate.getSeconds() - 1)
+          params = Object.assign(params, { dateTo: lastDate.toISOString(), dateFrom: '2012-01-01' })
+        }
+      } else if (more === 'after') {
+        const firstDocument = this.columns[indexCol].documents[0]
+        if (firstDocument) {
+          let firstDate = new Date(firstDocument.published)
+          firstDate.setSeconds(firstDate.getSeconds() + 1)
+          params = Object.assign(params, { dateFrom: firstDate.toISOString(), dateTo: 'now' })
+        }
+      }
+
+      this.columns[indexCol].processing = true
+
+      try {
+        const { documents, count } = await this.api.search(params)
+
+        this.$emit('saveToken', this.api.token)
+
+        const existingDocuments = this.columns[indexCol].documents
+
+        if (more === 'before') {
+          this.columns[indexCol].documents = existingDocuments.concat(documents)
+        } else if (more === 'after') {
+          this.columns[indexCol].documents = documents.concat(existingDocuments)
+        } else {
+          this.columns[indexCol].documents = documents
+          this.columns[indexCol].documentsCount = count
+        }
+
+        this.columns[indexCol].error = false
+      } catch (e) {
+        console.error(e)
+        this.columns[indexCol].error = true
+        this.columns[indexCol].documents = []
+      }
+
+      this.columns[indexCol].processing = false
+    },
+    async loadMoreDocuments (indexCol) {
+      await this.refreshColumn(indexCol, 'before')
+    },
+    startAutoRefresh () {
+      this.autoRefreshTimer = setInterval(() => {
+        Promise.all(
+          this.columns
+            .filter(column => !column.paramsOpen)
+            .map((column, i) => this.refreshColumn(i, 'after'))
+        )
+      }, this.autoRefreshDelay)
+    },
+    stopAutoRefresh () {
+      if (this.autoRefreshTimer) clearInterval(this.autoRefreshTimer)
+    },
+    saveColumns () {
+      const savedColumns = this.columns.map(column => {
+        return {
+          params: column.params,
+          name: column.name
+        }
+      })
+      this.$emit('saveColumns', savedColumns)
+    }
   }
 }
 </script>
