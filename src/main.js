2b16a3d66939bf21d052ef9a00f5a1d2dc2b3547
Jules Bonnard <julesbonnard@gmail.com>
2018-03-28 19:57:06 +0200

    add electron check for service worker

--- src/main.js
+++ src/main.js
@@ -9,13 +9,16 @@ new Vue({ // eslint-disable-line no-new
   render: h => h(App)
 }).$mount('#app')
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration)
-      // registration.pushManager.subscribe({userVisibleOnly: true})
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError)
+const userAgent = navigator.userAgent.toLowerCase()
+if (userAgent.indexOf(' electron/') === -1) {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration)
+        // registration.pushManager.subscribe({userVisibleOnly: true})
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
     })
-  })
+  }
 }
