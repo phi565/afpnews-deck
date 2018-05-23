5ecc176c7168c994e368bf01134e4be9e01cfe6c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-23 17:31:22 +0000

    Deploy julesbonnard/afpnews-deck to github.com/julesbonnard/afpnews-deck.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,19 @@
-importScripts("precache-manifest.ac5976c36709971811867d4663664ebc.js", "workbox-v3.2.0/workbox-sw.js");
+importScripts("precache-manifest.38a054e55d5bad5e129ed4a000939322.js", "workbox-v3.2.0/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.2.0"});
 workbox.skipWaiting()
 workbox.clientsClaim()
 
 workbox.routing.registerRoute(
   new RegExp('https://api.afp.com/objects/'),
-  workbox.strategies.staleWhileRevalidate()
-)
-
-workbox.routing.registerRoute(
-  new RegExp('https://api.afpforum.com/objects/'),
-  workbox.strategies.staleWhileRevalidate()
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'afpnews-assets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 24 * 60 * 60
+      })
+    ]
+  })
 )
 
 // self.addEventListener('push', event => {
