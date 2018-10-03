0fc82c9bbd93463c5f5dba827abdf0f920f5aba8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-03 17:51:40 +0000

    Deploy julesbonnard/afpnews-deck to github.com/julesbonnard/afpnews-deck.git:gh-pages

---
+++ precache-manifest.21b0c9c405c5fd34b1a3fd0ceb6f0b71.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "url": "fonts/0c012f227d51e98ec2d25a10169d0d58.0c012f22.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "url": "fonts/48222a8c287b5d336acd1886c8065a23.48222a8c.woff"
+  },
+  {
+    "revision": "7d9bed74ffa154cf5361",
+    "url": "css/app.197bb20e.css"
+  },
+  {
+    "revision": "db085786dad2f7e6c82b",
+    "url": "css/chunk-vendors.4669bd84.css"
+  },
+  {
+    "revision": "db085786dad2f7e6c82b",
+    "url": "js/chunk-vendors.06633b96.js"
+  },
+  {
+    "revision": "0517e764beb66117d20a",
+    "url": "css/login.8b7b03e0.css"
+  },
+  {
+    "revision": "0517e764beb66117d20a",
+    "url": "js/login.10fd9083.js"
+  },
+  {
+    "revision": "fe9b41085f6f9e35ec97",
+    "url": "css/tour.c01c69b6.css"
+  },
+  {
+    "revision": "fe9b41085f6f9e35ec97",
+    "url": "js/tour.d928b6d9.js"
+  },
+  {
+    "revision": "7f24272ea40da472714b",
+    "url": "css/viewer.3d0f03ad.css"
+  },
+  {
+    "revision": "7f24272ea40da472714b",
+    "url": "js/viewer.2b13e4a0.js"
+  },
+  {
+    "url": "fonts/4ee002e6e011bc250f137f5ce53c5405.4ee002e6.eot"
+  },
+  {
+    "revision": "923801c250977b667128",
+    "url": "js/about.5dc6f559.js"
+  },
+  {
+    "revision": "7d9bed74ffa154cf5361",
+    "url": "js/app.6ecb800a.js"
+  },
+  {
+    "url": "fonts/36315f6b56fdd22263c89438bc164a24.36315f6b.eot"
+  },
+  {
+    "url": "fonts/85c064570896c5881127b75fb909c37d.85c06457.woff2"
+  },
+  {
+    "url": "fonts/413b9e6d11282deea5a442df681fe92e.413b9e6d.woff"
+  },
+  {
+    "url": "fonts/3e3a5cfb26f79b3bb4bd1fbfa0a895e8.3e3a5cfb.eot"
+  },
+  {
+    "url": "fonts/913f4605c9771c6529a1e69d6124f81e.913f4605.woff2"
+  },
+  {
+    "url": "fonts/1600d9219cf8269494c6d404ad427c32.1600d921.woff"
+  },
+  {
+    "url": "fonts/6e0c661d1fb58c3132c438819057950d.6e0c661d.eot"
+  },
+  {
+    "url": "fonts/b7030b6d85524f1e2497e7b7f5572076.b7030b6d.woff2"
+  },
+  {
+    "url": "fonts/22a45ece31487721c99f98aedef08fd4.22a45ece.woff"
+  },
+  {
+    "revision": "2fc89e31f2c8c4890198240ced0a164a",
+    "url": "index.html"
+  },
+  {
+    "revision": "923801c250977b667128",
+    "url": "css/about.439d7ecc.css"
+  },
+  {
+    "revision": "8c1255eedf01c90a7d18a87d4e0c557d",
+    "url": "CNAME"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,32 @@
-importScripts("precache-manifest.5653a49248cb967bc7a5b7ab8649ef18.js", "workbox-v3.3.0/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "workbox-v3.3.0"});
-workbox.skipWaiting()
-workbox.clientsClaim()
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-workbox.routing.registerRoute(
-  new RegExp('https://api.afp.com/objects/'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'afpnews-assets',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-        maxAgeSeconds: 24 * 60 * 60
-      })
-    ]
-  })
-)
+importScripts("workbox-v3.6.2/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.2"});
 
-// self.addEventListener('push', event => {
-//   const title = 'Get Started With Workbox'
-//   const options = {
-//     body: event.data.text()
-//   }
-//   event.waitUntil(self.registration.showNotification(title, options))
-// })
+importScripts(
+  "precache-manifest.21b0c9c405c5fd34b1a3fd0ceb6f0b71.js"
+);
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest)
+workbox.core.setCacheNameDetails({prefix: "afpnews-deck"});
 
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/https:\/\/api.afp.com\/objects\//, workbox.strategies.staleWhileRevalidate({ "cacheName":"afpnews-assets", plugins: [new workbox.expiration.Plugin({"maxEntries":50,"maxAgeSeconds":86400,"purgeOnQuotaError":false})] }), 'GET');
