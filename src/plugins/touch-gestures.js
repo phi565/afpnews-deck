49f1b77be259b33feca738e2a3553444324b83ea
Jules Bonnard <julesbonnard@gmail.com>
2018-10-03 23:25:11 +0200

    fix touch action on hammer

--- src/plugins/touch-gestures.js
+++ src/plugins/touch-gestures.js
@@ -3,4 +3,6 @@ import { VueHammer } from 'vue2-hammer'
 import Hammer from 'hammerjs'
 
 delete Hammer.defaults.cssProps.userSelect // allow user to select text on desktop
+Hammer.defaults.touchAction = 'pan-y'
+
 Vue.use(VueHammer)
