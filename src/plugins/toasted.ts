4ed7fae7375c9c09b97d053d603d4c8297f1df95
Jules Bonnard <julesbonnard@gmail.com>
2019-02-20 18:44:37 +0100

    Add console.error along with toast

--- src/plugins/toasted.ts
+++ src/plugins/toasted.ts
@@ -3,11 +3,13 @@ import Toasted from 'vue-toasted'
 
 Vue.use(Toasted)
 
-Vue.toasted.register('error', (payload: ErrorEvent): string => {
-  if (!payload.message) {
+Vue.toasted.register('error', (error: ErrorEvent): string => {
+  // eslint-disable-next-line no-console
+  console.error(error)
+  if (!error.message) {
     return 'Oops.. Something Went Wrong..'
   }
-  return `Oops.. ${payload.message}`
+  return `Oops.. ${error.message}`
 }, {
   type: 'error',
   position: 'bottom-center',
