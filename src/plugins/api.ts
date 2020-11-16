168773b69f0d1b161c5c6c0fd4afde968ffa977b
Jules Bonnard <julesbonnard@gmail.com>
2020-11-16 19:18:29 +0100

    switch to API-core and ondeck client credentials

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -2,7 +2,8 @@ import AfpNews from 'afpnews-api'
 import store from '@/store'
 
 const afpNews = new AfpNews({
-  customAuthUrl: 'https://3o3qoiah2e.execute-api.eu-central-1.amazonaws.com/afpdeck',
+  baseUrl: 'https://afp-apicore-prod.afp.com',
+  customAuthUrl: 'https://3o3qoiah2e.execute-api.eu-central-1.amazonaws.com/ondeck',
   saveToken: token => {
     if (token) {
       store.commit('setToken', token)
