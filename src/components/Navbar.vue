7b6123a7746d62fbdca762a01b1cef84191e2855
Thomas KLEINHANS <kleinhans.thomas@gmail.com>
2020-11-17 15:56:26 +0100

    set navbar item into uppercase

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -91,6 +91,8 @@ export default {
         .navbar-end{
             background: $dark;
             .navbar-item{
+                text-transform: uppercase;
+                
                 &:hover{
                     background-color: $dark-hovered !important;
                 }
