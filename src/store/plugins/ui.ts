7e2f372e81c583b5204f5c2115e235fe502af7c4
Thomas KLEINHANS <kleinhans.thomas@gmail.com>
2020-11-26 11:30:22 +0100

    add the plugin which scroll to right

--- src/store/plugins/ui.ts
+++ src/store/plugins/ui.ts
@@ -3,19 +3,19 @@ import { Store } from 'vuex'
 import State from '@/store/state'
 
 export const ui = (store: Store<State>) => {
-  // store.subscribe(async ({ type }: { type: string }, state: State) => {
-  //   switch (type) {
-  //     case 'addColumn':
-  //       await Vue.nextTick()
-  //       const lastColumn = document.querySelector('.column:last-of-type')
-  //       if (lastColumn) {
-  //         lastColumn.scrollIntoView({
-  //           behavior: 'smooth',
-  //           block: 'end'
-  //         })
-  //       }
-  //       break
-  //     default:
-  //   }
-  // })
+  store.subscribe(async ({ type }: { type: string }, state: State) => {
+    switch (type) {
+      case 'addColumn':
+        await Vue.nextTick()
+        const lastColumn = document.querySelector('.column:last-of-type')
+        if (lastColumn) {
+          lastColumn.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end'
+          })
+        }
+        break
+      default:
+    }
+  })
 }
