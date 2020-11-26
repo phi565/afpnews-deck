4c8d8f30f043b684df93154563fc0a08b9687854
Thomas KLEINHANS <kleinhans.thomas@gmail.com>
2020-11-26 15:29:29 +0100

    disable automatic scroll to right on add column

--- src/store/plugins/ui.ts
+++ src/store/plugins/ui.ts
@@ -4,18 +4,18 @@ import State from '@/store/state'
 
 export const ui = (store: Store<State>) => {
   store.subscribe(async ({ type }: { type: string }, state: State) => {
-    switch (type) {
-      case 'addColumn':
-        await Vue.nextTick()
-        const lastColumn = document.querySelector('.column:last-of-type')
-        if (lastColumn) {
-          lastColumn.scrollIntoView({
-            behavior: 'smooth',
-            block: 'end'
-          })
-        }
-        break
-      default:
-    }
+    // switch (type) {
+    //   case 'addColumn':
+    //     await Vue.nextTick()
+    //     const lastColumn = document.querySelector('.column:last-of-type')
+    //     if (lastColumn) {
+    //       lastColumn.scrollIntoView({
+    //         behavior: 'smooth',
+    //         block: 'end'
+    //       })
+    //     }
+    //     break
+    //   default:
+    // }
   })
 }
